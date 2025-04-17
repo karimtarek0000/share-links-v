6c17f577f825fb29d2513f803a55ebcbc618244e
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-04-17 17:07:45 +0200

    fix(preview.vue): add account creation and login links to the layout

--- layouts/preview.vue
+++ layouts/preview.vue
@@ -2,5 +2,23 @@
 	<UContainer class="min-h-screen flex flex-col items-center justify-center">
 		<AppHeader :hide="true" class="w-full" />
 		<slot />
+		<div class="text-center mt-20">
+			<p class="text-gray-600 dark:text-gray-400">
+				Don't have an account?
+				<NuxtLink
+					to="/auth/signup"
+					class="font-medium text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-blue-600 hover:brightness-110 transition-all"
+				>
+					Create Account
+				</NuxtLink>
+				or
+				<NuxtLink
+					to="/auth/login"
+					class="font-medium text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-blue-600 hover:brightness-110 transition-all"
+				>
+					Login
+				</NuxtLink>
+			</p>
+		</div>
 	</UContainer>
 </template>
