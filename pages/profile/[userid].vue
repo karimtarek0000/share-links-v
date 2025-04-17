e35b864ea90411c7f55ffbe2f866eb25d3d26a57
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-04-17 16:56:49 +0200

    fix(profile/[userid].vue): implement profile data fetching and reactive state management

--- pages/profile/[userid].vue
+++ pages/profile/[userid].vue
@@ -1,14 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+definePageMeta({
+	layout: 'preview',
+})
+
+const { getProfile } = useProfileApi()
+const { detectPlatform } = useSocialPlatforms()
+const toast = useToast()
+const { params } = useRoute()
+
+// Create reactive form state
+const userData = reactive<UserData>({
+	name: '',
+	bio: '',
+	profileImage: null,
+	socials: [],
+})
+
+// This for make sure data will getting after redirect to the page
+await useAsyncData(async () => {
+	try {
+		const { body } = await getProfile(params.userid as string)
+		userData.name = body.name || ''
+		userData.bio = body.bio || ''
+		userData.profileImage = body.img || null
+		userData.socials = body.social_links.map((link: string) => ({
+			platform: detectPlatform(link).platform,
+			url: link,
+			icon: detectPlatform(link).icon,
+		}))
+	} catch (error: any) {
+		toast.add({
+			title: error.message || 'Error fetching profile data',
+			description: 'Failed to retrieve your profile data',
+			color: 'error',
+			icon: 'i-mdi-alert',
+		})
+	}
+})
+</script>
 
 <template>
-	<ProfilePreview
-		:userData="{
-			name: 'ssds',
-			bio: 'This is a sample bio.',
-			profileImage: null,
-			socials: [],
-		}"
-	/>
+	<ProfilePreview :userData="userData" />
 </template>
-
-<style scoped></style>
