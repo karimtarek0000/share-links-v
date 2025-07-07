102abcac9f778136c1ba9b23be81715a6851f7bb
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-07-07 22:18:15 +0300

    fix(nuxt.config.ts): update viewport meta tag to restrict maximum scale

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,10 @@ export default defineNuxtConfig({
       titleTemplate: '%s | Share Profile Links',
       meta: [
         { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1, maximum-scale=1',
+        },
         { name: 'description', content: 'Share your profile links easily.' },
       ],
       link: [
