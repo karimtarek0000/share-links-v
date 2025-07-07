a334b397c984ade17b505c0bb72edb2e40dd7793
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2025-07-07 17:48:35 +0300

    refactor(index.vue): standardize formatting and improve readability in template section

--- pages/index.vue
+++ pages/index.vue
@@ -2,39 +2,43 @@
 const profileFormRef = useTemplateRef('profileForm')
 
 definePageMeta({
-	middleware: 'auth',
+  middleware: 'auth',
+})
+
+useSeoMeta({
+  title: 'Dashboard',
 })
 </script>
 
 <template>
-	<div class="max-w-6xl mx-auto">
-		<!-- Main layout container -->
-		<div class="flex flex-col md:flex-row gap-8 md:gap-12">
-			<!-- Preview Section (Left on desktop, Top on mobile) -->
-			<div
-				class="w-full md:w-2/5 flex justify-center sticky max-sm:top-[300px]"
-			>
-				<ProfilePreview
-					:userData="
-						profileFormRef?.userData || {
-							name: '',
-							bio: '',
-							profileImage: null,
-							socials: [],
-						}
-					"
-				/>
-			</div>
+  <div class="max-w-6xl mx-auto">
+    <!-- Main layout container -->
+    <div class="flex flex-col md:flex-row gap-8 md:gap-12">
+      <!-- Preview Section (Left on desktop, Top on mobile) -->
+      <div
+        class="w-full md:w-2/5 flex justify-center sticky max-sm:top-[300px]"
+      >
+        <ProfilePreview
+          :userData="
+            profileFormRef?.userData || {
+              name: '',
+              bio: '',
+              profileImage: null,
+              socials: [],
+            }
+          "
+        />
+      </div>
 
-			<!-- Form Section (Right on desktop, Bottom on mobile) -->
-			<div class="w-full md:w-3/5 z-20">
-				<ProfileForm
-					ref="profileForm"
-					class="max-h-[78vh] py-2 px-1 overflow-auto custom-scrollbar"
-				/>
-			</div>
-		</div>
-	</div>
+      <!-- Form Section (Right on desktop, Bottom on mobile) -->
+      <div class="w-full md:w-3/5 z-20">
+        <ProfileForm
+          ref="profileForm"
+          class="max-h-[78vh] py-2 px-1 overflow-auto custom-scrollbar"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style></style>
